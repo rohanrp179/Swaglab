214497a7ab3c8cea6515a2d574bf45819926d9d7
rohanrp179 <rohan.r.pawar179@gmail.com>
2024-01-24 23:58:17 +0530

    browser utils general use methods

--- src/main/java/com/utils/BrowserUtil.java
+++ src/main/java/com/utils/BrowserUtil.java
@@ -1,5 +1,49 @@
 package com.utils;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BrowserUtil {
+	
+	private WebDriver wd;
+	private WebDriverWait wait;
+	
+	public BrowserUtil(WebDriver wd) {
+		super();
+		this.wd = wd;
+		wait = new WebDriverWait(wd, Duration.ofSeconds(30));
+	}
+	
+	public void openWebSite(String url) {
+		wd.get(url);
+	}
+	
+	public void maximizeBrowser() {
+		wd.manage().window().maximize();
+	}
+	
+	public void enterText(By locator, String textToEnter) {
+		WebElement ele = wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		ele.clear();
+		ele.sendKeys(textToEnter);
+	}
+	
+	public void clickOn(By locator) {
+		WebElement ele = wait.until(ExpectedConditions.elementToBeClickable(locator));
+		ele.click();
+	}
+
+
+	
+	
+	
+	
+	
+	
 
 }
